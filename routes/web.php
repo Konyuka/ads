5c22cf4a49f808ba37fb4743e93bd63378e0d88c
konyuka <michaelsaiba84@gmail.com>
2023-03-18 11:24:42 +0300

    push to submain

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,12 @@ use App\Http\Controllers\AdController;
 |
 */
 
+
+// submain push
+
 Route::get('/property/{url}', [AdController::class, 'index'])
     ->name('index');
 
-    
 
 // Route::get('property/{url}/details', 'propertyController@details')->name('properties.details');    
 
